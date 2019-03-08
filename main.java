035d5661383474164928118f433153f56404eef8
ilavisharma <lavisharma796@gmail.com>
2019-03-09 00:42:59 +0530

    create grid and start button

--- main.java
+++ main.java
@@ -2,30 +2,40 @@ import java.awt.*;
 import javax.swing.*;
 
 class Grid extends Frame {
-    Button but[] = new Button[300];
-    int x = 0, y = 0, z = 1;
+    int rows, cols;
+    Button but[][];
+    int x = 0, y = 0;
 
     Grid() {
         JFrame fr = new JFrame();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        double width = screenSize.getWidth();
-        double height = screenSize.getHeight();
+        // Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        // int width = (int) screenSize.getWidth();
+        // int height = (int) screenSize.getHeight();
+        cols = 70;
+        rows = 33;
 
         fr.setTitle("Grid");
-        fr.setSize(500, 500);
+        fr.setSize(1366, 768);
         fr.setLayout(null);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        for (int i = 0; i < but.length; i++) {
-            but[i] = new Button();
-            but[i].setBounds(x, y, 20, 20);
-            fr.add(but[i]);
-
-            if (x >= width) {
-                x = 0;
-
+        but = new Button[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                but[i][j] = new Button();
+                but[i][j].setBounds(x, y, 20, 20);
+                fr.add(but[i][j]);
+                if (x >= 1300) {
+                    x = 0;
+                    y = y + 20;
+                } else
+                    x = x + 20;
             }
         }
+
+        JButton start = new JButton("Start");
+        start.setBounds(10, y + 10, 100, 30);
+        fr.add(start);
 
         fr.setVisible(true);
     }
