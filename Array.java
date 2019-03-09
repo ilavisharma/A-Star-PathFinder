7c808e405a18a12a0cbb16e6b22ab1645c84a35d
ilavisharma <lavisharma796@gmail.com>
2019-03-09 23:14:10 +0530

    add push function

--- Array.java
+++ Array.java
@@ -1,3 +1,9 @@
 class Array {
-
+    public static void push(GridCell[] array, GridCell cell) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = cell;
+            }
+        }
+    }
 }
