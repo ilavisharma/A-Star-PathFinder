7330126003e061ed619cf2c228aae34f437dc7e9
ilavisharma <lavisharma796@gmail.com>
2019-03-10 13:01:43 +0530

    remove array class

--- Array.java
+++
@@ -1,9 +0,0 @@
-class Array {
-    public static void push(GridCell[] array, GridCell cell) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = cell;
-            }
-        }
-    }
-}
